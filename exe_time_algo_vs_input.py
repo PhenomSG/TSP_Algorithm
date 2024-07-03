a616fe205c855c5e81b411f394d3dec4701de359
Sahaj <sahajguptarocks@gmail.com>
2024-07-04 01:39:14 +0530

    mem graph

--- exe_time_algo_vs_input.py
+++ exe_time_algo_vs_input.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-# Define the data based on your inputs
 algorithms = [
     "backtracking.cpp",
     "bitmask_dp.cpp",
@@ -11,7 +10,6 @@ algorithms = [
     "reduced_matrix.cpp"
 ]
 
-# Memory usage for each algorithm across different inputs
 memory_usage = {
     "backtracking.cpp": [4896, 4836, 4844, 4848],
     "bitmask_dp.cpp": [5008, 4948, 4956, 4960],
@@ -22,10 +20,7 @@ memory_usage = {
     "reduced_matrix.cpp": [5016, 4948, 4956, 4960]
 }
 
-# Inputs (x-axis labels)
 inputs = ['Input 2', 'Input 3', 'Input 4', 'Input 5']
-
-# Plotting the data
 plt.figure(figsize=(12, 8))
 
 for alg in algorithms:
@@ -38,8 +33,5 @@ plt.legend()
 plt.grid(True)
 plt.tight_layout()
 
-# Save the plot
 plt.savefig('memory_usage_vs_inputs.png')
-
-# Show the plot
 plt.show()
